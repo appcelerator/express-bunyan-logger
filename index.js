ac0c6f9d99668d52c5a79a1faad2d3c75e805a47
Tony Pujals <tony@subfuzion.com>
2016-05-07 07:00:30 -0700

    use uuid v1 for time ordering

--- index.js
+++ index.js
@@ -194,9 +194,8 @@ function defaultLevelFn(status, err) {
 }
 
 
-
 function defaultGenReqId(req) {
-  var requestId = uuid.v4();
+  var requestId = uuid.v1();
   req.id = requestId;
   return requestId;
 }
